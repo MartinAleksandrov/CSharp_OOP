8f491c64a9cf395f7f13f1c479907c74659830a0
Martin Aleksandrov <martin_20.05@abv.bg>
2023-06-18 20:50:54 +0300

    All tasks from Inheritance lab

---
+++ Inheritance _LAB/CustomStack/StackOfStrings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty()
+        {
+            return Count == 0;
+        }
+        public void AddRange(IEnumerable<string> collection)
+        {
+            foreach (var item in collection)
+            {
+                this.Push(item);
+            }
+        }
+    }
+}
